189098a5e9ef16783d8dd17f3ccdd546743503ae
Eslam030 <93558896+Eslam030@users.noreply.github.com>
2023-02-04 16:31:17 +0200

    Update map.dart

--- where_map_foreach/map.dart
+++ where_map_foreach/map.dart
@@ -1,8 +1,18 @@
 void main(List<String> args) {
   // use map to convert every grade with A , B , C , D , F
   Map<String, int> grades = {'Ahmed': 75, 'Youssef': 82, 'Sherif': 90};
-  Map<String, String> gradesWithLetters;
+  Map<String, String> gradesWithLetters = grades.map((key, value) {
+    if (value >= 85 && value <= 100) {
+      return MapEntry(key, 'A');
+    } else if (value > 75 && value < 85)
+      return MapEntry(key, 'B');
+    else if (value > 65 && value <= 75)
+      return MapEntry(key, 'C');
+    else if (value >= 50 && value <= 65)
+      return MapEntry(key, 'D');
+    else
+      return MapEntry(key, 'F');
+  });
+  print(gradesWithLetters);
   // output should be {'Ahmed': 'C', 'Youssef': 'B', 'Sherif': 'A'}
-  
-
 }
